576ad7fe3ad18246d180cf397877807cd69a36f4
unwire\tkn <unwire\tkn@0766824d-1b24-0410-8b38-d1198a962fe6>
2009-04-21 15:49:50 +0000

    Added benchmark against soap4r
    git-svn-id: https://svn.unwire.dk/svnruby/handsoap/trunk@2452 0766824d-1b24-0410-8b38-d1198a962fe6

---
+++ tests/benchmark_test.rb
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+require 'handsoap'
+require 'soap/wsdlDriver'
+require 'soap/header/simplehandler'
+require 'benchmark'
+
+# handsoap mappings:
+
+class TestService < Handsoap::Service
+  endpoint :uri => 'http://127.0.0.1:8088/mockGoogleSearchBinding', :version => 1
+  map_method :do_spelling_suggestion => "urn:doSpellingSuggestion"
+  on_create_document do |doc|
+    doc.alias 'urn', "urn:GoogleSearch"
+    doc.alias 'xsi', "http://www.w3.org/2001/XMLSchema-instance"
+  end
+  def do_spelling_suggestion(key, phrase)
+    invoke("urn:doSpellingSuggestion") do |message|
+      message.add "key" do |k|
+        k.set_attr "xsi:type", "xsd:string"
+        k.add key
+      end
+      message.add "phrase" do |k|
+        k.set_attr "xsi:type", "xsd:string"
+        k.add phrase
+      end
+    end
+  end
+
+end
+
+# soap4r mappings:
+
+def make_soap4r
+  SOAP::WSDLDriverFactory.new('http://127.0.0.1:8088/mockGoogleSearchBinding?WSDL').create_rpc_driver('GoogleSearchService', 'GoogleSearchPort')
+end
+
+def make_handsoap
+  TestService.new
+end
+
+service_4 = make_soap4r
+service_h = make_handsoap
+
+# TestService.logger = $stdout
+# service_4.wiredump_dev = $stdout
+
+times = ARGV[0].to_i
+if times < 1
+  times = 1
+end
+puts "Benchmarking #{times} calls ..."
+Benchmark.bm(10) do |x|
+  x.report("handsoap") do
+    (1..times).each {
+      service_h.do_spelling_suggestion("foo", "bar")
+    }
+  end
+  x.report("soap4r") do
+    (1..times).each {
+      service_4.doSpellingSuggestion("foo", "bar")
+    }
+  end
+end
+puts "---------------"
+puts "Legend:"
+puts "The user CPU time, system CPU time, the sum of the user and system CPU times,"
+puts "and the elapsed real time. The unit of time is seconds."
