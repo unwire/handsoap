3168b5811c91be7c96e646a1ebc6d420e479f0a5
unwire\tkn <unwire\tkn@0766824d-1b24-0410-8b38-d1198a962fe6>
2009-04-21 15:50:00 +0000

    restructured file layout
    git-svn-id: https://svn.unwire.dk/svnruby/handsoap/trunk@2457 0766824d-1b24-0410-8b38-d1198a962fe6

--- tests/benchmark_test.rb
+++ tests/benchmark_test.rb
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + "/../lib")
 require 'handsoap'
 require 'soap/wsdlDriver'
 require 'soap/header/simplehandler'
--- tests/service_test.rb
+++ tests/service_test.rb
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + "/../lib")
 require 'handsoap'
 
 class TestService < Handsoap::Service
