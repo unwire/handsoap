6f179c8e0fb3bbf67e8862addadb676a97a8da79
unwire\tkn <unwire\tkn@0766824d-1b24-0410-8b38-d1198a962fe6>
2009-06-18 08:38:24 +0000

    Updated parser-tests
    git-svn-id: https://svn.unwire.dk/svnruby/handsoap/trunk@2771 0766824d-1b24-0410-8b38-d1198a962fe6

--- tests/parser_test.rb
+++ tests/parser_test.rb
@@ -12,52 +12,42 @@ end
 # http://soap.amazon.com/schemas2/AmazonWebServices.wsdl
 class TestOfAmazonWebServicesConnection < Test::Unit::TestCase
   def test_can_connect_and_read_wsdl
-    spider = Handsoap::Parser::XsdSpider.new('http://soap.amazon.com/schemas2/AmazonWebServices.wsdl')
-    spider.process!
-    assert_kind_of Array, spider.results
-    assert spider.results.length > 0, "Didn't read definitions"
-    assert_kind_of Hash, spider.wsdl
-    assert_kind_of Nokogiri::XML::Document, spider.wsdl[:document]
-    # var_dump spider.wsdl[:document].namespaces
+    wsdl = Handsoap::Parser::Wsdl.read('http://soap.amazon.com/schemas2/AmazonWebServices.wsdl')
+    assert_kind_of Handsoap::Parser::Interface, wsdl.interfaces.first
   end
 end
 
 class TestOfAmazonWebServices < Test::Unit::TestCase
-  def get_spider
-    unless @spider
-      @spider = Handsoap::Parser::XsdSpider.new('http://soap.amazon.com/schemas2/AmazonWebServices.wsdl')
-      @spider.process!
+  def get_wsdl
+    unless @wsdl
+      @wsdl = Handsoap::Parser::Wsdl.read('http://soap.amazon.com/schemas2/AmazonWebServices.wsdl')
     end
-    return @spider
+    return @wsdl
   end
   def test_can_parse_services
-    wsdl = Handsoap::Parser::WSDL.new(get_spider)
-    services = wsdl.services
-    assert_kind_of Hash, services
-    assert_equal 'AmazonSearchService', services.keys[0]
-    amazon_search_service = services['AmazonSearchService']
-    assert_kind_of Hash, amazon_search_service
-    assert_equal 'AmazonSearchPort', amazon_search_service.keys[0]
+    wsdl = get_wsdl
+    services = wsdl.endpoints
+    assert_kind_of Array, services
+    assert_kind_of Handsoap::Parser::Endpoint, services.first
+    assert_equal 'AmazonSearchPort', services.first.name
+    assert_equal 'typens:AmazonSearchBinding', services.first.binding
   end
   def test_can_parse_port_types
-    wsdl = Handsoap::Parser::WSDL.new(get_spider)
-    port_types = wsdl.port_types
-    assert_kind_of Hash, port_types
-    assert_equal 'AmazonSearchPort', port_types.keys[0]
-    amazon_search_port = port_types['AmazonSearchPort']
-    assert_kind_of Hash, amazon_search_port
-    assert_equal 'UpcSearchRequest', amazon_search_port.keys[0]
-  end
-  def test_can_parse_messages
-    wsdl = Handsoap::Parser::WSDL.new(get_spider)
-    messages = wsdl.messages
-    assert_kind_of Hash, messages
-    assert_equal 'ShoppingCartResponse', messages.keys[0]
+    wsdl = get_wsdl
+    port_types = wsdl.interfaces
+    assert_kind_of Array, port_types
+    assert_kind_of Handsoap::Parser::Interface, port_types.first
+    assert_equal 'AmazonSearchPort', port_types.first.name
+    assert_kind_of Array, port_types.first.operations
+    assert_equal 'KeywordSearchRequest', port_types.first.operations.first.name
   end
   def test_can_parse_bindings
-    wsdl = Handsoap::Parser::WSDL.new(get_spider)
+    wsdl = get_wsdl
     bindings = wsdl.bindings
-    assert_kind_of Hash, bindings
+    assert_kind_of Array, bindings
+    assert_equal 'AmazonSearchBinding', bindings.first.name
+    assert_kind_of Array, bindings.first.actions
+    assert_equal 'KeywordSearchRequest', bindings.first.actions.first.name
   end
 end
 
@@ -65,76 +55,41 @@ end
 # http://www.thomas-bayer.com/names-service/soap?wsdl
 class TestOfThomasBayerNameServiceConnection < Test::Unit::TestCase
   def test_can_connect_and_read_wsdl
-    spider = Handsoap::Parser::XsdSpider.new('http://www.thomas-bayer.com/names-service/soap?wsdl')
-    spider.process!
-    assert_kind_of Array, spider.results
-    assert spider.results.length > 0, "Didn't read definitions"
-    assert_kind_of Hash, spider.wsdl
-    assert_kind_of Nokogiri::XML::Document, spider.wsdl[:document]
+    wsdl = Handsoap::Parser::Wsdl.read('http://www.thomas-bayer.com/names-service/soap?wsdl')
+    assert_kind_of Handsoap::Parser::Interface, wsdl.interfaces.first
   end
 end
 
 class TestOfThomasBayerNameService < Test::Unit::TestCase
-  def get_spider
-    unless @spider
-      @spider = Handsoap::Parser::XsdSpider.new('http://www.thomas-bayer.com/names-service/soap?wsdl')
-      @spider.process!
+  def get_wsdl
+    unless @wsdl
+      @wsdl = Handsoap::Parser::Wsdl.read('http://www.thomas-bayer.com/names-service/soap?wsdl')
     end
-    return @spider
+    return @wsdl
   end
   def test_can_parse_services
-    wsdl = Handsoap::Parser::WSDL.new(get_spider)
-    services = wsdl.services
-    assert_kind_of Hash, services
-    assert_equal 'NamesServiceService', services.keys[0]
-    name_service = services['NamesServiceService']
-    assert_kind_of Hash, name_service
-    assert_equal 'NamesServicePort', name_service.keys[0]
+    wsdl = get_wsdl
+    services = wsdl.endpoints
+    assert_kind_of Array, services
+    assert_kind_of Handsoap::Parser::Endpoint, services.first
+    assert_equal 'NamesServicePort', services.first.name
+    assert_equal 'tns:NamesServicePortBinding', services.first.binding
   end
   def test_can_parse_port_types
-    wsdl = Handsoap::Parser::WSDL.new(get_spider)
-    port_types = wsdl.port_types
-    assert_kind_of Hash, port_types
-    assert_equal 'NamesService', port_types.keys[0]
-    name_service = port_types['NamesService']
-    assert_kind_of Hash, name_service
-    assert_equal 'getNamesInCountry', name_service.keys[0]
-  end
-  def test_can_parse_messages
-    wsdl = Handsoap::Parser::WSDL.new(get_spider)
-    messages = wsdl.messages
-    assert_kind_of Hash, messages
-    assert_equal 'getNamesInCountry', messages.keys[0]
-    assert_kind_of Handsoap::Parser::Part, messages['getNamesInCountry']['parameters']
-    assert_equal 'getNamesInCountry', messages['getNamesInCountry']['parameters'].type.name
+    wsdl = get_wsdl
+    port_types = wsdl.interfaces
+    assert_kind_of Array, port_types
+    assert_kind_of Handsoap::Parser::Interface, port_types.first
+    assert_equal 'NamesService', port_types.first.name
+    assert_kind_of Array, port_types.first.operations
+    assert_equal 'getCountries', port_types.first.operations.first.name
   end
   def test_can_parse_bindings
-    wsdl = Handsoap::Parser::WSDL.new(get_spider)
+    wsdl = get_wsdl
     bindings = wsdl.bindings
-    assert_kind_of Hash, bindings
-  end
-  def test_can_parse_elements
-    wsdl = Handsoap::Parser::WSDL.new(get_spider)
-    elements = wsdl.elements
-    assert_kind_of Hash, elements
-    assert_equal '{http://namesservice.thomas_bayer.com/}getNameInfo', elements.keys[0]
-    assert_kind_of Handsoap::Parser::Element, elements['{http://namesservice.thomas_bayer.com/}getNameInfo']
-  end
-  def test_can_parse_types
-    wsdl = Handsoap::Parser::WSDL.new(get_spider)
-    types = wsdl.types
-    assert_kind_of Hash, types
-    assert_equal '{http://namesservice.thomas_bayer.com/}getNameInfo', types.keys[0]
-    assert_kind_of Handsoap::Parser::ComplexType, types['{http://namesservice.thomas_bayer.com/}getNameInfo']
+    assert_kind_of Array, bindings
+    assert_equal 'NamesServicePortBinding', bindings.first.name
+    assert_kind_of Array, bindings.first.actions
+    assert_equal 'getCountries', bindings.first.actions.first.name
   end
 end
-
-
-
-# spider = Handsoap::XsdSpider.new('http://soap.amazon.com/schemas2/AmazonWebServices.wsdl')
-# spider.process!
-# # var_dump spider.results
-
-# wsdl = Handsoap::WSDL.new(spider)
-# var_dump wsdl.messages.keys
-# # var_dump wsdl.port_types
