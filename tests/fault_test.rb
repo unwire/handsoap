60fdd078f897018ed3bdaed72f00d2d7f79c8281
troelskn <troelskn@gmail.com>
2009-08-05 16:43:15 +0200

    Added test for soap 1.2 faults

--- tests/fault_test.rb
+++ tests/fault_test.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'rubygems'
 require 'test/unit'
 
@@ -7,7 +8,7 @@ require 'handsoap/xml_query_front'
 require 'handsoap/service'
 
 class ParseFaultTestCase < Test::Unit::TestCase
-  def get_xml_document
+  def get_fault_11
     xml_doc = '<?xml version="1.0" encoding="UTF-8"?>
   <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
     <soap:Body>
@@ -20,12 +21,48 @@ class ParseFaultTestCase < Test::Unit::TestCase
   </soap:Envelope>'
     Handsoap::XmlQueryFront.parse_string(xml_doc, Handsoap.xml_query_driver)
   end
-  def test_can_parse_soap_fault
+  def get_fault_12
+    xml_doc = '<?xml version="1.0" ?>
+<env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope"
+            xmlns:rpc="http://www.w3.org/2003/05/soap-rpc">
+  <env:Body>
+   <env:Fault>
+     <env:Code>
+       <env:Value>env:Sender</env:Value>
+       <env:Subcode>
+        <env:Value>rpc:BadArguments</env:Value>
+       </env:Subcode>
+     </env:Code>
+     <env:Reason>
+      <env:Text xml:lang="en-US">Processing error</env:Text>
+      <env:Text xml:lang="cs">Chyba zpracování</env:Text>
+     </env:Reason>
+     <env:Detail>
+      <e:myFaultDetails
+        xmlns:e="http://travelcompany.example.org/faults">
+        <e:message>Name does not match card number</e:message>
+        <e:errorcode>999</e:errorcode>
+      </e:myFaultDetails>
+     </env:Detail>
+   </env:Fault>
+ </env:Body>
+</env:Envelope>'
+    Handsoap::XmlQueryFront.parse_string(xml_doc, Handsoap.xml_query_driver)
+  end
+  def test_can_parse_soap_fault_11
     envelope_namespace = "http://schemas.xmlsoap.org/soap/envelope/"
-    node = get_xml_document.xpath('/env:Envelope/env:Body/descendant-or-self::env:Fault', { 'env' => envelope_namespace })
+    node = get_fault_11.xpath('/env:Envelope/env:Body/descendant-or-self::env:Fault', { 'env' => envelope_namespace })
     fault = Handsoap::Fault.from_xml(node, :namespace => envelope_namespace)
     assert_kind_of Handsoap::Fault, fault
     assert_equal 'soap:Server', fault.code
     assert_equal 'Error while blackList account: the application does not exist', fault.reason
+  end
+  def test_can_parse_soap_fault_12
+    envelope_namespace = "http://www.w3.org/2003/05/soap-envelope"
+    node = get_fault_12.xpath('/env:Envelope/env:Body/descendant-or-self::env:Fault', { 'env' => envelope_namespace })
+    fault = Handsoap::Fault.from_xml(node, :namespace => envelope_namespace)
+    assert_kind_of Handsoap::Fault, fault
+    assert_equal 'env:Sender', fault.code
+    assert_equal 'Processing error', fault.reason
   end
 end
