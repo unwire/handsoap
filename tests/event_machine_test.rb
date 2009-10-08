81ab21565525161851aa0af28567fd7f475369d1
ydnar <ydnar@shaderlab.com>
2009-10-08 02:54:29 +0000

    Integration test for EventMachine-based service.

--- tests/event_machine_test.rb
+++ tests/event_machine_test.rb
@@ -52,6 +52,46 @@ class TestSocketServer
   self.start
 end
 
+
+class TestService < Handsoap::Service
+  endpoint :uri => "http://127.0.0.1:#{TestSocketServer.port}/", :version => 1
+
+  def on_create_document(doc)
+    doc.alias 'sc002', "http://www.wstf.org/docs/scenarios/sc002"
+    doc.find("Header").add "sc002:SessionData" do |s|
+      s.add "ID", "Client-1"
+    end
+  end
+
+  def on_response_document(doc)
+    doc.add_namespace 'ns', 'http://www.wstf.org/docs/scenarios/sc002'
+  end
+
+  def echo(text)
+    deferred = invoke('sc002:Echo') do |message|
+      message.add "text", text
+    end
+    deferred.callback {
+      soap_response = deferred.options['handsoap.soap_response']
+      deferred.options['echo.text'] = (soap_response.document/"//ns:EchoResponse/ns:text").to_s
+    }
+    deferred
+  end
+end
+
+
+SOAP_RESPONSE = <<-XML
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:sc0="http://www.wstf.org/docs/scenarios/sc002">
+   <soap:Header/>
+   <soap:Body>
+      <sc0:EchoResponse>
+         <sc0:text>I am living in the future.</sc0:text>
+      </sc0:EchoResponse>
+   </soap:Body>
+</soap:Envelope>
+XML
+
+
 class TestOfEventMachineDriver < Test::Unit::TestCase
   def driver
     :event_machine
@@ -79,6 +119,28 @@ OK".gsub(/\n/, "\r\n")
         assert_equal "Ruby", response.headers['SERVER']
         assert_equal "OK", response.body
         assert_equal 200, response.status
+        EventMachine.stop
+      }
+    }
+  end
+  
+  def test_service
+    TestSocketServer.reset!
+    TestSocketServer.responses << "HTTP/1.1 200 OK
+Server: Ruby
+Connection: close
+Content-Type: application/xml
+Content-Length: #{SOAP_RESPONSE.size}
+Date: Wed, 19 Aug 2009 12:13:45 GMT
+
+".gsub(/\n/, "\r\n") + SOAP_RESPONSE
+
+    Handsoap.http_driver = :event_machine
+    
+    EventMachine.run {
+      deferred = TestService.echo("I am living in the future.")
+      deferred.callback {
+        assert_equal "I am living in the future.", deferred.options['echo.text']
         EventMachine.stop
       }
     }
