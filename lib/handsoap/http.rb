unwire/handsoap

commit a0fdcf5603e149cbf372d6f47d09edfdd70e1c65

switched to lines.each for ruby 1.8/1.9 compatibility

lib/handsoap/http.rb
@@ -419,7 +419,7 @@ module Handsoap
     def self.parse_headers(raw)
       header = Hash.new([].freeze)
       field = nil
-      raw.gsub(/^(\r\n)+|(\r\n)+$/, '').each {|line|
+      raw.gsub(/^(\r\n)+|(\r\n)+$/, '').lines.each {|line|
         case line
         when /^([A-Za-z0-9!\#$%&'*+\-.^_`|~]+):\s*(.*?)\s*\z/om
           field, value = $1, $2

commit c4553057139dc3ec8378b5ee2bd980656eac8f5e

Fixed a bug introduced by commit "a0fdcf5603e149cbf372d6f47d09edfdd70e1c65". The String.lines does not work with ruby 1.8.x

lib/handsoap/http.rb
@@ -419,7 +419,7 @@ module Handsoap
     def self.parse_headers(raw)
       header = Hash.new([].freeze)
       field = nil
-      raw.gsub(/^(\r\n)+|(\r\n)+$/, '').lines.each {|line|
+      raw.gsub(/^(\r\n)+|(\r\n)+$/, '').each {|line|
         case line
         when /^([A-Za-z0-9!\#$%&'*+\-.^_`|~]+):\s*(.*?)\s*\z/om
           field, value = $1, $2