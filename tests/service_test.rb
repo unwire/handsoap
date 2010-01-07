9d5398b877aad342b0aaa2954cb0b60bfab468bb
Michel Barbosa <me@cicaboo.com>
2010-01-07 22:35:32 +0800

    added test with regards to follow redirects support

---
+++ tests/service_test.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'test/unit'
+
+$LOAD_PATH << "#{File.dirname(__FILE__)}/../lib/"
+require "handsoap"
+
+class TestFollowRedirects < Test::Unit::TestCase
+  def test_follow_redirects
+    assert !Handsoap.follow_redirects?
+    Handsoap.follow_redirects!
+    assert Handsoap.follow_redirects?
+  end
+  
+  def test_max_redirects
+    assert_equal Handsoap.max_redirects, 1
+    Handsoap.max_redirects = 10
+    assert_equal Handsoap.max_redirects, 10
+  end
+end
